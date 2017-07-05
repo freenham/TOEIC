4fdb6d9894cebbe74e823c17d0474b709c0eb34a
JungHyun <freenham@gmail.com>
2017-07-05 18:17:36 +0900

    53-55 완성

--- TOEIC/src/toeic/ToeicRunner.java
+++ TOEIC/src/toeic/ToeicRunner.java
@@ -436,16 +436,43 @@ public class ToeicRunner {
 		bogi[1]="Reserve a seat";
 		bogi[2]="Order dinner";
 		bogi[3]="Purchase tickets";
-		script = "W : Good afternoon James. I'm curious to know what enterment would be offered tonight?"
+		script = "W : Good afternoon James. I'm curious to know what entertainment will be offered tonight?"
 				+ "\n M : This evening a musician will be performing in our auditorium on the second level of the criuse ship."
 				+ "\n W : Hopefully, the show will be comparable to the jazz music that we heared yesterday. That was wonderful."
 				+ "\n M : I think you'll find this evening entertainment equally as enjoyable."
 				+ "\n W : Excellent. And when will the show begin?"
-				+ "\n M : At 8 pm. After diner has been served. If you'd like watch it, I suggest you arriving at 7:30 pm. Because there is limited seating available and we expect a full audience."
+				+ "\n M : At 8 pm. After diner has been served. If you'd like watch it, I suggest arriving at 7:30 pm. Because there's limited seating available and we expect a full audience."
 				+ "\n W : Sounds good. I'll do that.";
 		Part3 p55  = new Part3(55,"What does the man suggest the woman do?",script,bogi,"B");
 		System.out.println(p55);
 		
+		bogi[0]="The time of a departure";
+		bogi[1]="Entertainment options";
+		bogi[2]="The reason for a delay";
+		bogi[3]="Ticket prices";
+		Part3 p56  = new Part3(56,"What are the speakers mainly discussing?",script,bogi,"B");
+		System.out.println(p56);
+		
+		bogi[0]="A musical performance";
+		bogi[1]="A popular play";
+		bogi[2]="A magic show";
+		bogi[3]="A dance recital";
+		Part3 p57  = new Part3(74,"According to the woman, what took place yesterday?",script,bogi,"B");
+		System.out.println(p57);
+		
+		bogi[0]="Arrive early";
+		bogi[1]="Reserve a seat";
+		bogi[2]="Order dinner";
+		bogi[3]="Purchase tickets";
+		script = "W : Good afternoon James. I'm curious to know what entertainment will be offered tonight?"
+				+ "\n M : This evening a musician will be performing in our auditorium on the second level of the criuse ship."
+				+ "\n W : Hopefully, the show will be comparable to the jazz music that we heared yesterday. That was wonderful."
+				+ "\n M : I think you'll find this evening entertainment equally as enjoyable."
+				+ "\n W : Excellent. And when will the show begin?"
+				+ "\n M : At 8 pm. After diner has been served. If you'd like watch it, I suggest arriving at 7:30 pm. Because there's limited seating available and we expect a full audience."
+				+ "\n W : Sounds good. I'll do that.";
+		Part3 p58  = new Part3(58,"What does the man suggest the woman do?",script,bogi,"B");
+		System.out.println(p58);
 		
 		
 	}
