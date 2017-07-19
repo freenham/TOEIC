fe033b1d07043d07315ec300ee6f0414d2efc6e8
JungHyun <freenham@gmail.com>
2017-07-19 12:24:54 +0900

    4 초안

--- TOEIC/src/test04/Script.java
+++ TOEIC/src/test04/Script.java
@@ -4,22 +4,26 @@ public class Script {
 	String[] script;
 	
 	public Script() {
-		script = new String[100];
+		script = new String[101];
 		for(int i=0;i<100;i++) {
 			script[i]="";
 		}
-		script[0] = "(A) He's washing off(up x) his face.\n"
+		script[1] = "(A) He's washing off(up x) his face.\n"
 				+ "(B) He's drinking from a fountain(분수).\n"
 				+ "(C) He's emptying water from a bottle.\n"
 				+ "(D) He's turning off a faucet(수도꼭지).\n";
-		script[1] = "(A) They're adjusting some tools.\n"
+		script[2] = "(A) They're adjusting some tools.\n"
 				+ "(B) They're pointing at a conveyor belt.\n"
 				+ "(C) They're standing near a piece of machinery.\n"
 				+ "(D) They're carrying some boxes.\n";
-		script[2] = "(A) He's standing rear of a vehicle.\n"
-				+ "(B) He's setting a tire and to the trunk.\n"
-				+ "(C) He's parking a car in front of a entracnce.\n"
+		script[3] = "(A) He's standing at the rear of a vehicle.\n"
+				+ "(B) He's setting a tire into the trunk.(그는 타이어를 트렁크에 넣고있다.)\n"
+				+ "(C) He's parking a car in front of an entracnce.\n"
 				+ "(D) He's opening the door for a passanger.\n";
+		script[4] = "(A) The women are sitted next to each other.\n"
+				+ "(B) Coffee's being import to cups.\n"
+				+ "(C) One of the woman is delivering the mail.\n"
+				+ "(D) Documents are being printed out.\n";
 	}
 	
 	public String toString(){
